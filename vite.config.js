0d9618cd131c0f4656e2621b647830684b43e5fc
Bruno Pasqual <brunoh.pasqual0@gmail.com>
2023-07-25 09:33:00 -0300

    teste imagens

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'R-audiophile-ecommerce',
+  base: '/R-audiophile-ecommerce',
 });
